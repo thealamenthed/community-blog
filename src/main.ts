c2f2fab10dbfb8ead527ffc2a3afa39b3d090f8a
thealamenthed <dalila.boualemfr@gmail.com>
2023-05-07 16:41:19 +0200

    feat(registration): install axios

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,12 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 // Connect to router
 import router from './router'
+import axios from 'axios'
 
 import '@/assets/main.css'
+
+axios.defaults.withCredentials = true // définie le comportement d'axios pour chaque requet
+axios.defaults.baseURL = 'https://simpledev.one/api' // définie une BaseURL pour connecter l'api côté server
 
 const app = createApp(App)
 
