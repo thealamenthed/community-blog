fb7314e2a9be422aeaae6bbe3663d06c7505c72f
thealamenthed <dalila.boualemfr@gmail.com>
2023-05-18 16:45:08 +0200

    feat(registration): get data from user in dashboard

--- src/pages/DashboardView.vue
+++ src/pages/DashboardView.vue
@@ -1,10 +1,14 @@
 <template>
-<div class="text-lg text-slate-600">
-    dashboard !
+<div class="p-6 text-6xl text-slate-900">
+    {{user.getUser.email}}
 </div>
 
 </template>
 
 
 <script setup>
+import { useUserStore } from '@/stores/user'
+
+const user = useUserStore()
+
 </script>
