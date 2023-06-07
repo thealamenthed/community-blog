8731a0908777f2163160f648c75698ac481fd47d
thealamenthed <dalila.boualemfr@gmail.com>
2023-06-07 18:33:27 +0200

    feat(create-post): new component CreatePostView, add link in navBar

---
+++ src/pages/CreatePostView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    class="overflow-hidden antialiased bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400 -ms-overflow-style: none"
+  >
+    <div class="px-4">
+      <div
+        class="flex items-center justify-center max-w-2xl p-16 mx-auto my-16 bg-white rounded-lg"
+      >
+        <h1 class="text-2xl font-medium">{{ title }}</h1>
+      </div>
+    </div>
+  </div>
+
+  <!-- ====== Blog Section Start -->
+  <section class="pt-20 pb-10 lg:pt-[120px] lg:pb-20">
+    <div class="container mx-auto">
+      <div class="flex flex-wrap justify-center -mx-4">
+        <div class="w-full px-4">
+          <div class="mx-auto mb-[60px] max-w-[510px] text-center lg:mb-20"></div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- ====== Blog Section End -->
+</template>
+
+<script setup></script>
