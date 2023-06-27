c04c21224f7cf897e73719d36b601c61a805a906
thealamenthed <dalila.boualemfr@gmail.com>
2023-06-27 19:38:23 +0200

    feat(edit): add form in modal

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -175,22 +175,58 @@
                         <DialogTitle as="h3" class="text-base font-semibold leading-6 text-gray-900"
                           >Editer {{ post.title }}</DialogTitle
                         >
-                        <div class="mt-2">
-                          <p class="text-sm text-gray-500">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
-                            amet labore.
-                          </p>
-                        </div>
+                        <form
+                          @submit.prevent="onSubmit"
+                          method="post"
+                          class="max-w-xl mx-auto mt-16 sm:mt-20"
+                        >
+                          <input type="hidden" name="post_id" :value="form.post_id" />
+                          <input type="hidden" name="user_id" :value="form.user_id" />
+
+                          <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+                            <div class="sm:col-span-2">
+                              <label
+                                for="title"
+                                class="flex text-sm font-semibold leading-6 text-gray-900"
+                                >Title</label
+                              >
+                              <div class="mt-2.5">
+                                <input
+                                  v-model="form.title"
+                                  type="text"
+                                  name="title"
+                                  autocomplete="title"
+                                  class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                              </div>
+                            </div>
+
+                            <div class="sm:col-span-2">
+                              <label
+                                for="message"
+                                class="flex text-sm font-semibold leading-6 text-gray-900"
+                                >Content</label
+                              >
+                              <div class="mt-2.5">
+                                <textarea
+                                  v-model="form.content"
+                                  name="content"
+                                  rows="4"
+                                  class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                              </div>
+                            </div>
+                          </div>
+                          <div class="mt-10">
+                            <button
+                              type="submit"
+                              class="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            >
+                              Update
+                            </button>
+                          </div>
+                        </form>
                       </div>
-                    </div>
-                    <div class="mt-5 sm:mt-6">
-                      <button
-                        type="button"
-                        class="inline-flex justify-center w-full px-3 py-2 text-sm font-semibold text-white bg-indigo-600 rounded-md shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                        @click="open = false"
-                      >
-                        Go back to dashboard
-                      </button>
                     </div>
                   </DialogPanel>
                 </TransitionChild>
