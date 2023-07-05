034142906c15ea3b52ebc2377afb7dd6a0d8ae59
thealamenthed <dalila.boualemfr@gmail.com>
2023-07-05 17:54:48 +0200

    refactor(navBar): NavBar component

--- src/components/TheNavBar.vue
+++ src/components/TheNavBar.vue
@@ -1,154 +1,170 @@
 <template>
-  <!-- When the mobile menu is open, add `overflow-hidden` to the `body` element to prevent double scrollbars -->
-  <Popover as="template" v-slot="{ open }">
-    <header
-      :class="[
-        open ? 'fixed inset-0 z-40 overflow-y-auto' : '',
-        'bg-white shadow-sm lg:static lg:overflow-y-visible'
-      ]"
+  <header class="bg-white">
+    <nav
+      class="flex items-center justify-between p-6 mx-auto max-w-7xl gap-x-6 lg:px-8"
+      aria-label="Global"
     >
-      <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-        <div class="relative flex justify-between lg:gap-8 xl:grid xl:grid-cols-12">
-          <div class="flex md:absolute md:inset-y-0 md:left-0 lg:static xl:col-span-2">
-            <!-- logo -->
-
-            <div class="flex m-3 text-sm font-semibold leading-6 text-gray-900">
-              <a class="flex items-center" href="/">
-                <HomeIcon class="block w-6 h-6 m-2 text-gray-900" />
-                Home
-              </a>
-            </div>
-
-            <div v-if="!user.loggedIn" class="flex items-center flex-shrink-0">
-              <a class="flex m-3 text-sm font-semibold leading-6 text-gray-900" href="/register">
-                <UserPlusIcon class="block w-6 h-6 m-2 text-slate-500" />
-                Register
-              </a>
-            </div>
-
-            <div v-if="!user.loggedIn" class="flex items-center flex-shrink-0">
-              <a href="login" class="text-sm font-semibold leading-6 text-gray-900"
-                >Log in <span aria-hidden="true">&rarr;</span></a
-              >
-            </div>
-
-            <div v-if="user.loggedIn" class="flex items-center flex-shrink-0">
-              <a class="flex m-3 text-sm font-semibold leading-6 text-gray-900" href="/create-post">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  class="w-6 h-6"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
-                  />
-                </svg>
-                Poster
-              </a>
-            </div>
-
-            <div
-              v-if="user.loggedIn"
-              @click.prevent="user.logout"
-              class="flex items-center flex-shrink-0"
-            >
-              <a href="." class="text-sm font-semibold leading-6 text-gray-900">Log out</a>
-            </div>
-          </div>
-        </div>
+      <div class="flex lg:flex-1">
+        <a href="/" class="-m-1.5 p-1.5">
+          <span class="sr-only">Your Company</span>
+          <img
+            class="w-auto h-8"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            alt=""
+          />
+        </a>
       </div>
 
-      <PopoverPanel as="nav" class="lg:hidden" aria-label="Global">
-        <div class="max-w-3xl px-2 pt-2 pb-3 mx-auto space-y-1 sm:px-4">
-          <a
-            v-for="item in navigation"
-            :key="item.name"
-            :href="item.href"
-            :aria-current="item.current ? 'page' : undefined"
-            :class="[
-              item.current ? 'bg-gray-100 text-gray-900' : 'hover:bg-gray-50',
-              'block rounded-md py-2 px-3 text-base font-medium'
-            ]"
-            >{{ item.name }}</a
+      <div class="flex items-center justify-end flex-1 gap-x-6">
+        <a
+          v-if="!user.loggedIn"
+          href="login"
+          class="hidden lg:block lg:text-sm lg:font-semibold lg:leading-6 lg:text-gray-900"
+          >Log in</a
+        >
+        <a
+          v-if="user.loggedIn"
+          class="hidden m-3 mr-3 -mx-3 text-sm font-semibold leading-6 text-gray-900 lg:flex lg:block"
+          href="/create-post"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
           >
-        </div>
-        <div class="pt-4 pb-3 border-t border-gray-200">
-          <div class="flex items-center max-w-3xl px-4 mx-auto sm:px-6">
-            <div class="flex-shrink-0">
-              <img class="w-10 h-10 rounded-full" :src="user.imageUrl" alt="" />
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+            />
+          </svg>
+          Poster
+        </a>
+
+        <a v-if="user.loggedIn" href="/dashboard" class="flex-shrink-0 hidden -mx-3 lg:block group">
+          <div class="flex items-center">
+            <div>
+              <span
+                v-if="src"
+                class="inline-block rounded-full h-9 w-9"
+                :style="{ backgroundImage: 'url(' + user.getUser?.avatar?.thumbnail_url + ')' }"
+              />
+              <span v-else class="inline-block bg-purple-500 rounded-full h-9 w-9" />
             </div>
             <div class="ml-3">
-              <div class="text-base font-medium text-gray-800">{{ user.name }}</div>
-              <div class="text-sm font-medium text-gray-500">{{ user.email }}</div>
+              <p class="text-sm font-medium text-gray-700 group-hover:text-gray-900">
+                {{ user.getUser?.name }}
+              </p>
+              <p class="text-xs font-medium text-gray-500 group-hover:text-gray-700">
+                View profile
+              </p>
             </div>
-            <button
-              type="button"
-              class="flex-shrink-0 p-1 ml-auto text-gray-400 bg-white rounded-full hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-            >
-              <span class="sr-only">View notifications</span>
-              <BellIcon class="w-6 h-6" aria-hidden="true" />
-            </button>
           </div>
-          <div class="max-w-3xl px-2 mx-auto mt-3 space-y-1 sm:px-4">
-            <a
-              v-for="item in userNavigation"
-              :key="item.name"
-              :href="item.href"
-              class="block px-3 py-2 text-base font-medium text-gray-500 rounded-md hover:bg-gray-50 hover:text-gray-900"
-              >{{ item.name }}</a
-            >
+        </a>
+        <a
+          v-if="!user.loggedIn"
+          href="register"
+          class="px-3 py-2 text-sm font-semibold text-white bg-indigo-600 rounded-md shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >Sign up</a
+        >
+      </div>
+      <div class="flex lg:hidden">
+        <button
+          type="button"
+          class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+          @click="mobileMenuOpen = true"
+        >
+          <span class="sr-only">Open main menu</span>
+          <Bars3Icon class="w-6 h-6" aria-hidden="true" />
+        </button>
+      </div>
+    </nav>
+    <Dialog as="div" class="lg:hidden" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
+      <div class="fixed inset-0 z-10" />
+      <DialogPanel
+        class="fixed inset-y-0 right-0 z-10 w-full px-6 py-6 overflow-y-auto bg-white sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
+      >
+        <div class="flex items-center gap-x-6">
+          <a href="/" class="-m-1.5 p-1.5">
+            <span class="sr-only">Your Company</span>
+            <img
+              class="w-auto h-8"
+              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+              alt=""
+            />
+          </a>
+          <a
+            v-if="!user.loggedIn"
+            href="register"
+            class="px-3 py-2 ml-auto text-sm font-semibold text-white bg-indigo-600 rounded-md shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >Sign up</a
+          >
+          <a
+            v-if="user.loggedIn"
+            href="."
+            class="px-3 py-2 ml-auto text-sm font-semibold text-white bg-indigo-600 rounded-md shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >Log out</a
+          >
+          <button
+            type="button"
+            class="-m-2.5 rounded-md p-2.5 text-gray-700"
+            @click="mobileMenuOpen = false"
+          >
+            <span class="sr-only">Close menu</span>
+            <XMarkIcon class="w-6 h-6" aria-hidden="true" />
+          </button>
+        </div>
+        <div class="flow-root mt-6">
+          <div class="-my-6 divide-y divide-gray-500/10">
+            <div class="py-6 space-y-2">
+              <a
+                v-for="item in navigation"
+                :key="item.name"
+                :href="item.href"
+                class="block px-3 py-2 -mx-3 text-base font-semibold leading-7 text-gray-900 rounded-lg hover:bg-gray-50"
+                >{{ item.name }}</a
+              >
+            </div>
+            <div class="py-6">
+              <a
+                v-if="!user.loggedIn"
+                href="login"
+                class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >Log in</a
+              >
+              <a
+                v-if="user.loggedIn"
+                href="create-post"
+                class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >Poster</a
+              >
+              <a
+                v-if="user.loggedIn"
+                href="dashboard"
+                class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >{{ user.getUser?.name }}
+                <p class="text-xs font-medium text-gray-500 group-hover:text-gray-700">
+                  View profile
+                </p>
+              </a>
+            </div>
           </div>
         </div>
-      </PopoverPanel>
-    </header>
-  </Popover>
+      </DialogPanel>
+    </Dialog>
+  </header>
 </template>
 
 <script setup>
-import {
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuItems,
-  Popover,
-  PopoverButton,
-  PopoverPanel
-} from '@headlessui/vue'
-import { MagnifyingGlassIcon } from '@heroicons/vue/20/solid'
-import {
-  Bars3Icon,
-  BellIcon,
-  XMarkIcon,
-  HomeIcon,
-  UserPlusIcon,
-  ArrowLongUpIcon
-} from '@heroicons/vue/24/outline'
-
 import { useUserStore } from '@/stores/user'
+import { ref } from 'vue'
+import { Dialog, DialogPanel } from '@headlessui/vue'
+import { Bars3Icon, XMarkIcon } from '@heroicons/vue/24/outline'
 
 const user = useUserStore()
 
-// const user = {
-//   name: 'Chelsea Hagon',
-//   email: 'chelsea.hagon@example.com',
-//   imageUrl:
-//     'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
-// }
-
-const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Calendar', href: '#', current: false },
-  { name: 'Teams', href: '#', current: false },
-  { name: 'Directory', href: '#', current: false }
-]
-const userNavigation = [
-  { name: 'Your Profile', href: '#' },
-  { name: 'Settings', href: '#' },
-  { name: 'Sign out', href: '#' }
-]
+const mobileMenuOpen = ref(false)
 </script>
